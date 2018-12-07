d9e600a80643fe4d001aafcd8c5500b94165c845
Alexandros Nicolaou <alnicolaou96@gmail.com>
2018-12-06 22:43:12 -0500

    Night Mode Button added/ Javascript in process

--- GuyWithAKureg/JavaScript.js
+++ GuyWithAKureg/JavaScript.js
@@ -1,1 +1,6 @@
-﻿
+﻿function NightMode() {
+    var n = document.getElementById("backgroundColor");
+    if (n.style.backgroundColor === "rgb(255, 95, 134);") {
+        n.style.backgroundColor = "rgb(26, 33, 35)";
+    }
+}
