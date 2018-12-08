770bf525017676e12c8a739cd549025ab00eb748
Alexandros Nicolaou <alnicolaou96@gmail.com>
2018-12-08 09:40:06 -0500

    night mode is now functioning

--- GuyWithAKureg/JavaScript.js
+++ GuyWithAKureg/JavaScript.js
@@ -1,10 +1,13 @@
-﻿function NightMode() {
+﻿var night = false;
+function NightMode() {
 
-    var n = document.getElementById("background");
-    if (n.style.backgroundColor !== "rgb(255, 95, 134);") {
-        console.log(n.style.backgroundColor);
+    var n = document.getElementById("background").style;
+    if (night === false) {
+        n.backgroundColor = "black";
+        night = true;
     }
-    else if (n.style.backgroundColor === "black") {
-        console.log("working");
+    else if (night === true) {
+        n.backgroundColor = "indianred";
+        night = false;
     }
 }
